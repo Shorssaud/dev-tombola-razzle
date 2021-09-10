b0b4462f54b158bb7b5c4ad80bab6281e4ecc84d
Shorssaud <shaun.orssaud@epitech.eu>
2021-09-10 10:44:00 +0200

    wip

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -2,32 +2,64 @@ import React from 'react';
 import logo from './react.svg';
 import './Home.css';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="Home">
-        <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to Razzle</h2>
-        </div>
-        <p className="Home-intro">
-          To get started, edit <code>src/App.js</code> or{' '}
-          <code>src/Home.js</code> and save to reload.
-        </p>
-        <ul className="Home-resources">
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-          </li>
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </li>
-          <li>
-            <a href="https://palmer.chat">Community Slack</a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+class Reservation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Title: null,
+            Association: null,
+            Domaine: null,
+            Ville: null,
+            CodePostal: null
+        };
 
-export default Home;
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange = async (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+        return(
+            <div style={{display:'flex', color:'red'}}>
+                <button onClick={() => alert('button clicked!')}>Click me!</button>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+                <form style={{display:'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+                    <label>
+                        Titre de Tombola:
+                        <input type="text" name="Titre de Tombola"checked={this.state.isGoing}
+                               onChange={this.handleInputChange} />
+                        <br />
+                        Nom de l'association:
+                        <input type="text" name="Nom de l'association" checked={this.state.isGoing}
+                               onChange={this.handleInputChange} />
+                        <br />
+                        Domaine:
+                        <input type="text" name="Domaine" checked={this.state.isGoing}
+                               onChange={this.handleInputChange} />
+                        <br />
+                        Ville:
+                        <input type="text" name="Ville" checked={this.state.isGoing}
+                               onChange={this.handleInputChange} />
+                        <br />
+                        Code Postal:
+                        <input type="text" name="Code Postal" checked={this.state.isGoing}
+                               onChange={this.handleInputChange} />
+                    </label>
+                    <button type="submit" value="Submit" checked={this.state.isGoing} onClick={async () => {await this.handleInputChange}}>
+                        Submit
+                    </button>
+            </form>
+        );
+    }
+}
+export default Reservation;
